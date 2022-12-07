72300ad430d5b39d186212f5b7af617e10ddd9d1
eodovo <eodovo@naver.com>
2022-12-07 18:17:41 +0900

    effect 추가

--- src/pages/TemplateDetail.js
+++ src/pages/TemplateDetail.js
@@ -1,22 +1,44 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 import "./TemplateDetail.css";
 
 function TemplateDetail(props) {
+  const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 2000);
+    console.log(2);
+    return () => {
+      console.log(1);
+      // clearTimeout(a);
+    };
+  });
+
   let { id } = useParams();
-  const imgSrc = props.title[id].img;
+  const product = props.title[id];
 
   return (
     <div className="row gx-0 content">
+      <span>{count}</span>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        버튼
+      </button>
+      <div>{show === true ? <div>ㅎㅇ ㅋㅋ 2초안에사면 공짜</div> : null}</div>
       <div className="detailImageBox col-md-auto">
-        <img src={imgSrc} alt="productDetail img" />
+        <img src={product.img} alt="productDetail img" />
       </div>
       <div className="col-md-auto">
         <div className="detailText col-md-auto">
           <dl>
-            <dt>{props.title[id].title}</dt>
-            <dd>{props.title[id].content}</dd>
-            <dd>${props.title[id].price.toLocaleString()}</dd>
+            <dt>{product.title}</dt>
+            <dd>{product.content}</dd>
+            <dd>${product.price.toLocaleString()}</dd>
             <button className="btn btn-danger">ORDER!</button>
           </dl>
         </div>
