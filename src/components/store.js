fa9da5313c0500f574ca8ac5e1c4f76d84fd20c9
eodovo <eodovo@naver.com>
2022-12-22 05:41:23 +0900

    장바구니 수정

--- src/components/store.js
+++ src/components/store.js
@@ -1,26 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-let user = createSlice({
-  name: "user",
-  // initialState: "kim",
-  initialState: {
-    name: "kim",
-    age: 20,
-  },
-  reducers: {
-    changeName(state) {
-      state.name = "pack";
-    },
-  },
-  aded(state) {
-    state.age += 1;
-  },
-});
-
-export let { changeName } = user.actions;
-export let { aded } = user.actions;
-//user.actions = state 변경 함수들이 남음. 구조분해할당 문법 사용
-
 let cart = createSlice({
   name: "cart",
   initialState: [],
@@ -58,9 +37,10 @@ let cart = createSlice({
   },
 });
 export let { increase, addItem, decrease, deleteItem } = cart.actions;
+
+//cart.actions = state 변경 함수들이 남음. 구조분해할당 문법 사용
 export default configureStore({
   reducer: {
     cart: cart.reducer,
-    user: user.reducer,
   },
 });
